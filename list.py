43ff61552623b75bed613343dc29d778dea59560
Zabir Hasan <dominicancongo@gmail.com>
2024-09-11 21:29:06 +0600

    first commit

---
+++ list.py
@@ -0,0 +1,15 @@
+li = ["Osama", "On obama", "Futai dilo boma"]
+
+print(li)
+print(li[0])
+print(li[1])
+print(li[2])
+
+numbers = [9, 11, 2001]
+print(numbers)
+print(numbers[-1])
+print(numbers[-2])
+
+alist = [100, 200 ,300, 400, 500]
+print( alist[::-1])
+print("sapnu puas ")
